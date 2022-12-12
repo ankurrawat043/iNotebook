6baa46460dfc50e6478f768dace3c055d1231c40
ANKUR RAWAT <106335896+ankurrawat043@users.noreply.github.com>
2022-12-12 11:31:21 +0530

    lecture 45

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose= require('mongoose');
-const mongoURI='mongodb://localhost:27017/test';
+const mongoURI='mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.1';
 const connectToMongo =()=>{
 mongoose.connect(mongoURI, ()=>{
     console.log("connected to mongo sucessfully");
